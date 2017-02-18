0eeb7f38e633cabc3dd9cdf31cedf06c40a4af5e
sinwar <premsinwar4@gmail.com>
2017-02-18 11:16:59 +0530

    initialise

---
+++ 6.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define MP make_pair
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define PER(i,a,b) for(int i=b;i>=a;i--)
+#define X first
+#define Y second
+ 
+ 
+ 
+//cost
+#define MOD 10000000
+#define MOD_INV 1000000006
+#define MAX 100009
+
+typedef long long ll;
+
+
+int main()
+{
+	return 0;
+}
